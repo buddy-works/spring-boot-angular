2c340c44898f36c43f07a9220e5ba7fc0445bf8c
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2020-02-20 09:50:01 +0100

    Implement new greetings (#1)

--- src/main/java/works/buddy/springbootangular/GreetingController.java
+++ src/main/java/works/buddy/springbootangular/GreetingController.java
@@ -10,13 +10,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class GreetingController {
 
-    private static final String template = "Hello, %s!";
+    private static final String template = "Hello, %s! from Spring";
     private final AtomicLong counter = new AtomicLong();
 
     @CrossOrigin
     @GetMapping("/greeting")
     public Greeting greeting(@RequestParam(required=false, defaultValue="World") String name) {
-        System.out.println("==== in greeting ====");
         return new Greeting(counter.incrementAndGet(), String.format(template, name));
     }
 
